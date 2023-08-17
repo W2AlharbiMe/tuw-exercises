5d6aa2107a6a10a9e067f5ae9f7261a7a7982c92
W2AlharbiMe <w2.alharbi.me@gmail.com>
2023-08-17 15:46:49 +0300

    add column

--- src/main/java/com/example/exercises/Model/Librarian.java
+++ src/main/java/com/example/exercises/Model/Librarian.java
@@ -1,9 +1,6 @@
 package com.example.exercises.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
@@ -23,18 +20,22 @@ public class Librarian {
 
     @NotEmpty(message = "the name field is required.")
     @Size(min = 5, message = "the name must be more than 4 chars.")
+    @Column(columnDefinition = "Varchar(120) not null")
     private String name;
 
     @NotEmpty(message = "the username field is required.")
     @Size(min = 4, message = "the username must be more than 3 chars.")
+    @Column(columnDefinition = "Varchar(120) not null unique")
     private String username;
 
     @NotEmpty(message = "the password field is required.")
     @Pattern(message = "the password must contain at least seven characters, at least one number and both lower and uppercase letters and special characters", regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")
+    @Column(columnDefinition = "Varchar(75) not null")
     private String password;
 
     @NotEmpty(message = "the email field is required.")
     @Email(message = "invalid email.")
+    @Column(columnDefinition = "Varchar(200) not null unique")
     private String email;
 
 }
